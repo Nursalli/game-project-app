5c4e198e600bf6611791954a1e28221136bf3361
Augyeris Lioga Seandrio <83108316+masegy@users.noreply.github.com>
2022-08-06 11:12:07 +0700

    Slicing/login register pages (#2)
    * update init
    
    * slicing: login and register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,43 @@
 function Login(props) {
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={props.onSignIn}>Sign In</button>
+    <div className="border shadow rounded w-50">
+      <form>
+        {/* <!-- Email input --> */}
+        <div className="form-outline mb-4 pt-4">
+          <label className="form-label" for="form2Example1">
+            Email
+          </label>
+          <input type="username" id="form2Example1" className="form-control" />
+        </div>
+
+        {/* <!-- Password input --> */}
+        <div className="form-outline mb-4">
+          <label className="form-label" for="form2Example2">
+            Password
+          </label>
+          <input type="password" id="form2Example2" className="form-control" />
+        </div>
+
+        {/* <!-- Submit button --> */}
+        <div className="d-flex w-100 justify-content-center">
+          <button
+            onClick={props.onSignIn}
+            className="btn btn-primary btn-block mb-4"
+          >
+            Login
+          </button>
+        </div>
+        <div className="d-flex w-100 justify-content-center">
+          <button
+            onClick={props.onSignIn}
+            className="btn btn-primary btn-block mb-4"
+          >
+            (G) Login with Google
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,67 @@
 function Register(props) {
   return (
-    <div>
-      <h1>Register</h1>
-      <button onClick={props.onSignUp}>Sign Up</button>
+    <div className="border shadow rounded w-50">
+      <form>
+        {/* <!-- Email input --> */}
+        <div className="form-outline mb-4 pt-4">
+          <label className="form-label" for="form2Example1">
+            Email
+          </label>
+          <input type="email" id="form2Example1" className="form-control" />
+        </div>
+
+        {/* <!-- First Name input --> */}
+        <div className="form-outline mb-4">
+          <label className="form-label" for="form2Example1">
+            First Name
+          </label>
+          <input type="firstname" id="form2Example1" className="form-control" />
+        </div>
+
+        {/* <!-- Last Name input --> */}
+        <div className="form-outline mb-4">
+          <label className="form-label" for="form2Example1">
+            Last Name
+          </label>
+          <input type="lastname" id="form2Example1" className="form-control" />
+        </div>
+
+        {/* <!-- Password input --> */}
+        <div className="form-outline mb-4">
+          <label className="form-label" for="form2Example2">
+            Password
+          </label>
+          <input type="password" id="form2Example2" className="form-control" />
+        </div>
+
+        {/* <!-- re-type password input --> */}
+        <div className="form-outline mb-4">
+          <label className="form-label" for="form2Example2">
+            Re-type Password
+          </label>
+          <input type="password" id="form2Example2" className="form-control" />
+        </div>
+
+        {/* <!-- Submit button --> */}
+        <div className="d-flex w-100 justify-content-center">
+          <button
+            onClick={props.onSignUp}
+            className="btn btn-primary btn-block mb-4"
+          >
+            Register
+          </button>
+        </div>
+        <div className="d-flex w-100 justify-content-center">
+          <button
+            onClick={props.onSignIn}
+            className="btn btn-primary btn-block mb-4"
+          >
+            (G) Sign Up with Google
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Register;

0e800a8273fb81c4104c55a8a2d3c3c133f44385
corwin.xyz <corwin.xyz@gmail.com>
2022-08-10 19:30:11 +0700

    Adding slicing/landing+gamelist

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ export { default as Footer } from "./Footer"
 export { default as Navbar } from "./Navbar"
 export { default as GameList } from "./gameComponent/GameList"
 export { default as GameHistory } from "./gameComponent/GameHistory"
-
+export { default as GameBox } from "./GameBox/Gamebox"
--- src/pages/GameList.jsx
+++ src/pages/GameList.jsx
@@ -1,7 +1,43 @@
+import { GameBox } from "../components/index";
+
 function GameList() {
   return (
-    <div>
-      <h1>Our Games</h1>
+    <div className="justify-content-around">
+      <div className="d-flex w-100 justify-content-center pt-5">
+            <p>Our Games</p>
+      </div>
+
+      <div class = "container-fluid d-flex justify-content-center">
+      <div class = "row">
+        <div class = "col-sm-4">
+          <GameBox gameName="Game 1"/>
+        </div>
+        <div class = "col-sm-4">
+          <GameBox gameName="Game 2"/>
+        </div>
+        <div class = "col-sm-4">
+          <GameBox gameName="Game 3"/>
+        </div>
+        <div class = "col-sm-4">
+          <GameBox gameName="Game 4"/>
+        </div>
+        <div class = "col-sm-4">
+          <GameBox gameName="Game 5"/>
+        </div>
+        <div class = "col-sm-4">
+          <GameBox gameName="Game 6"/>
+        </div>
+        <div class = "col-sm-4">
+          <GameBox gameName="Game 7"/>
+        </div>
+        <div class = "col-sm-4">
+          <GameBox gameName="Game 8"/>
+        </div>
+        <div class = "col-sm-4">
+          <GameBox gameName="Game 9"/>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }

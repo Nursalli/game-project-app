792c8ab907a77ff765a771f46f6c5faa4b0fff33
Augyeris Lioga Seandrio <83108316+masegy@users.noreply.github.com>
2022-08-12 22:13:18 +0700

    Handle/game details (#19)
    * game detail and integrate login/register
    
    * handle: redirect after login and procted routed and navbar
    
    * handle: game details
    
    * update: post to play-count
    
    * handle: other game except game suit

--- src/components/GameDetailsComponent/index.jsx
+++ src/components/GameDetailsComponent/index.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 function index(props) {
   return (
     <>
-      <div className="d-flex justify-content-between border listContainer">
+      <div className="d-flex justify-content-between border rounded listContainer">
         <div className="col d-flex justify-content-center align-items-center">
           <img
             src="https://static.vecteezy.com/system/resources/previews/000/691/497/large_2x/rock-paper-scissors-neon-icons-vector.jpg"
@@ -12,13 +12,16 @@ function index(props) {
           />
         </div>
 
-        <div className="col d-flex flex-column justify-content-center align-items-start">
-          <div className="">Player Vladi</div>
-          <div className="pb-2">@username</div>
-          <div className="border w-100 text-center">BadgeName</div>
+        <div
+          className="col d-flex flex-column justify-content-center align-items-start"
+          style={{ width: "100vw" }}
+        >
+          <div className="">{props.email}</div>
+          <div className="pb-2">@{props.name}</div>
+          <div className="border w-100 text-center">{props.BadgeName}</div>
         </div>
 
-        <div className="col d-flex align-items-center gamePoint">
+        <div className="col d-flex align-items-center gamePoint justify-content-end">
           <p className="value">
             <strong>{props.gamePoint}</strong>
           </p>
